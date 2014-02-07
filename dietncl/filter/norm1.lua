b527470a702d207c3b0a93e804a6a9a76675a445
Rafael Azevedo <araphawake@gmail.com>
2014-02-07 11:28:34 -0200

    maint: Update function break_ternary_compounds (parent)
    * dietncl/filter/norm1.lua: Updated function break_ternary_compounds (parent)
    which is now converting compound conditions (operator or) correctly.
    * tests/test-filter-norm1.lua: Added a new test for NF1.

--- dietncl/filter/norm1.lua
+++ dietncl/filter/norm1.lua
@@ -16,9 +16,9 @@ for more details.
 You should have received a copy of the GNU General Public License along
 with DietNCL.  If not, see <http://www.gnu.org/licenses/>.  ]]--
 
--- The PRENORM1-5 filters simplify links and connectors from a given NCL
--- document. This filter, First Normal Form (NF1), guarantees that for any
--- ABS program S there is an equivalent program S' such that each link L
+-- The NORM1-2 filters simplify links and connectors from a given NCL
+-- document. This filter NORM1, under the First Normal Form (NF1), guarantees 
+-- that for any ABS program S there is an equivalent program S' such that each link L
 -- of S' has condition and action degrees zero.
 --
 -- Depends: PRENORM5.
@@ -37,52 +37,62 @@ _ENV = nil
 --
 
 local function break_ternary_compounds (parent)
-    print(parent)
-
+    
+    
     if parent.operator == 'or' and #parent == 3 then
-
+        
         local new_compound = {}
         local statement
+        local child_list = {} 
 
         for i, child in ipairs (parent) do
-
-            child = xml.remove (parent, child)
-
-            if child:tag () == 'compoundCondition' then
-                new_compound[i] = xml.new ('compoundCondition')
-                new_compound[i].operator = 'and'
-                parent:insert(new_compound[i])
-                new_compound[i]:insert (child)
-            else
-                statement = xml.remove (parent, child)
-            end
-
+            child_list[i] = child
         end
-
-        for index, child in ipairs (new_compound) do
-            child:insert (statement)
+        
+        for index, child in ipairs (child_list) do
+            if child:tag() == 'assessmentStatement' or child:tag() == 'compoundStatement' then
+                statement = xml.remove (child:parent(), child)
+            end
+        end
+        
+        for index, child in ipairs (child_list) do
+            if child:tag () == 'compoundCondition' then
+                new_compound[index] = xml.new ('compoundCondition')
+                new_compound[index].operator = 'and'
+                parent:insert(new_compound[index])
+                child = xml.remove (parent, child)
+                new_compound[index]:insert (child)
+                local copy = statement:clone()
+                new_compound[index]:insert (copy)
+            end
         end
 
     elseif parent.operator == 'and' and #parent == 3 then
-
+        
 
     elseif #parent <= 2 then
         -- TODO: Fix make_ternary_compounds (parent[1]), parent[1] is not being accepted.
         if parent[1] == 'simpleCondition' or parent[2] == 'simpleCondition' then
             return
         elseif parent[1]:tag() == 'compoundCondition' then
-            make_ternary_compounds (parent[1])
+            break_ternary_compounds (parent[1])
         elseif parent[2]:tag() == 'compoundCondition' then
-            make_ternary_compounds (parent[2])
+            break_ternary_compounds (parent[2])
+        end
+    end
+    
+    for index, child in ipairs (parent) do
+        if child and child:tag() == 'compoundCondition' and child.operator == 'or' then
+            break_ternary_compounds (child)
         end
     end
 
     for index, child in ipairs (parent) do
-        if child[1]:tag() == 'compoundCondition' then
-            break_ternary_compounds (child[1])
+            if child[1] and child[1]:tag() == 'compoundCondition'  then
+                break_ternary_compounds (child[1])
+            end
         end
-    end
-
+    
 end
 
 
@@ -92,8 +102,7 @@ function filter.apply (ncl)
     for conn in ncl:gmatch ('causalConnector') do
         break_ternary_compounds (conn)
     end
-
-    print (ncl)
+    
     return ncl
 
 end
