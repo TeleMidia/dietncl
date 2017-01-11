09865105f4b0772202db6232063b110a7b8482aa
Jorge Pumar Dodsworth <jorgepd@telemidia.puc-rio.br>
2017-01-11 13:07:01 -0200

    maint: Added a new descriptor filter

---
+++ dietncl/filter/descriptor.lua
@@ -0,0 +1,90 @@
+--[[ filter.unused_media -- Remove unused medias.
+     Copyright (C) 2013-2017 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of DietNCL.
+
+DietNCL is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+DietNCL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with DietNCL.  If not, see <http://www.gnu.org/licenses/>.  ]]--
+
+local print = print
+local ipairs = ipairs
+local filter = {}
+local xml = require ('dietncl.xmlsugar')
+_ENV = nil
+
+---
+-- Removes all `<descriptor>` elements from NCL document.
+--
+-- @module dietncl.filter.descriptor
+---
+
+---
+-- Avoid property clash. Insert the property with the highest priority in
+-- an NCL document.
+
+local function insert_properties (ncl, media, name, value)
+   if name=="id" or name=="region" then
+      return
+   end
+
+   for par in media:children() do
+      if name == par.name then
+	 return
+      end
+   end
+
+   local prop = ncl.new('property')
+   prop.name = name
+   prop.value = value
+   media:insert (prop)
+end
+
+---
+-- Removes all descriptors from NCL document. The function proceeds by
+-- transforming the attributes of each media into equivalent parameters
+-- of the associated descriptor.
+-- @param ncl NCL document (root element).
+-- @return the modified NCL document (root element).
+---
+function filter.apply (ncl)
+   for desc in ncl:gmatch ('descriptor') do
+      local list = {ncl:match ('media', 'descriptor', desc.id)}
+      if #list == 0  then
+	 goto done 
+      end
+
+      for _,media in ipairs (list) do
+	 for descpar in desc:children() do
+	    insert_properties (ncl, media, descpar.name, descpar.value)	  
+	 end
+
+
+	 for name, value in desc:attributes() do
+	    insert_properties (ncl, media, name, value)
+	 end
+
+	 media.descriptor = nil
+      end
+      
+      ::done::
+      xml.remove (desc:parent(), desc)
+   end
+   
+   print (ncl)
+   return (ncl)
+end
+
+
+return filter
+
+
---
+++ tests/test-filter-descriptor.lua
@@ -0,0 +1,168 @@
+--[[ Copyright (C) 2013-2017 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of DietNCL.
+
+DietNCL is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+DietNCL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with DietNCL.  If not, see <http://www.gnu.org/licenses/>.  ]]--
+
+local assert = assert
+local print = print
+local dietncl = require ('dietncl')
+local xml = require ('dietncl.xmlsugar')
+local filter = require ('dietncl.filter.descriptor')
+
+_ENV = nil
+
+
+-- Remove descriptor and descriptorParam and insert the correct properties
+-- to media element.
+
+local str = [[
+<ncl>
+  <head>
+    <descriptor id='d1' top='5%'>
+      <descriptorParam name='top' value='6%'/>
+      <descriptorParam name='left' value='10%'/>
+    </descriptor>
+  </head>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png' descriptor='d1'/>
+  </body>
+</ncl>
+]]
+
+local ncl = dietncl.parsestring (str)
+assert (filter.apply (ncl))
+
+local result = dietncl.parsestring ([[
+<ncl>
+  <head/>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png'>
+      <property name='top' value='6%'/>
+      <property name='left' value='10%'/>
+    </media>
+  </body>
+</ncl>
+]])
+
+assert (xml.equal (ncl, result))
+
+
+-- Ignores the descriptorParam that clashes with the existent media property.
+
+local str = [[
+<ncl>
+  <head>
+    <descriptor id='d1' top='5%'>
+      <descriptorParam name='top' value='6%'/>
+      <descriptorParam name='left' value='10%'/>
+    </descriptor>
+  </head>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png' descriptor='d1'>
+      <property name='top' value='15%'/>
+    </media>
+  </body>
+</ncl>
+]]
+
+local ncl = dietncl.parsestring (str)
+assert (filter.apply (ncl))
+
+local result = dietncl.parsestring ([[
+<ncl>
+  <head/>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png'>
+      <property name='top' value='15%'/>
+      <property name='left' value='10%'/>
+    </media>
+  </body>
+</ncl>
+]])
+
+assert (xml.equal (ncl, result))
+
+
+-- Descriptor with no parameters.
+
+local str = [[
+<ncl>
+  <head>
+    <descriptor id='d1'>
+      <descriptorParam name='top' value='6%'/>
+      <descriptorParam name='left' value='10%'/>
+    </descriptor>
+  </head>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png' descriptor='d1'/>
+  </body>
+</ncl>
+]]
+
+local ncl = dietncl.parsestring (str)
+assert (filter.apply (ncl))
+
+local result = dietncl.parsestring ([[
+<ncl>
+  <head/>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png'>
+      <property name='top' value='6%'/>
+      <property name='left' value='10%'/>
+    </media>
+  </body>
+</ncl>
+]])
+
+assert (xml.equal (ncl, result))
+
+
+-- Remove descriptor that is not being referenced.
+
+local str = [[
+<ncl>
+  <head>
+    <descriptor id='d1' top='5%'>
+      <descriptorParam name='top' value='6%'/>
+      <descriptorParam name='left' value='10%'/>
+    </descriptor>
+  </head>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png'/>
+  </body>
+</ncl>
+]]
+
+local ncl = dietncl.parsestring (str)
+assert (filter.apply (ncl))
+
+local result = dietncl.parsestring ([[
+<ncl>
+  <head/>
+  <body>
+    <port id='p' component='m'/>
+    <media id='m' src='test.png'/>
+  </body>
+</ncl>
+]])
+
+assert (xml.equal (ncl, result))
